af48ba8e5c297808904938139d5c178dc5578811
timohaeckel <timohaeckel@gmx.de>
2016-12-09 18:38:17 +0100

    Startet generator.

---
+++ src/main/java/de/haeckel/development/weihnachtsturnier/generator/Generator.java
@@ -0,0 +1,15 @@
+package de.haeckel.development.weihnachtsturnier.generator;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * Created by Timo Haeckel on 09.12.2016.
+ */
+public class Generator extends Application{
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
